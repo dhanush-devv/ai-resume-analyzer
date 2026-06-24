53fff2f5ec35b337174205e73fb1cc0a0168295b
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-06-24 08:52:37 +0530

    commit

--- backend/src/services/aiService.js
+++ backend/src/services/aiService.js
@@ -1,7 +1,6 @@
 const { GoogleGenAI } = require('@google/genai')
 const { z } = require('zod')
 const { zodToJsonSchema } = require('zod-to-json-schema')
-const chromium = require('@sparticuz/chromium');
 const puppeteer = require('puppeteer-core');
 const ai = new GoogleGenAI({
   apiKey: process.env.GOOGLE_GENAI_API_KEY,
